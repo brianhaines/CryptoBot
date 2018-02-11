4cb303bc460526813415db8906b25fa2ce927050
DevOnLinux <b.ge.haines@gmail.com>
2018-02-11 00:55:09 -0500

    added database connections and env vars in settings

--- mainTrading_CB2.py
+++ mainTrading_CB2.py
@@ -3,8 +3,12 @@ from settings import API_KEY
 from settings import API_SECRET
 from settings import API_PASSPHRASE
 from settings import PRODUCTS
+from settings import DBNAME
+from settings import DB_USER
+from settings import DB_PASSWORD
 from event import TickEvent
 from decimal import Decimal, getcontext, ROUND_HALF_DOWN
+import pymysql.cursors
 import logging
 import queue
 import threading
@@ -52,7 +56,18 @@ class CryptoBot(gdax.WebsocketClient):
 def trade(events):
 	latestPrices= {}
 	latestPrices['USD-USD'] = {"bid": Decimal("1.0"), "ask": Decimal("1.0"),"last":Decimal("1.0")}
+	arbitrages = {}
+	arbitrages['ETH-BTC'] = {'in':Decimal('0.0000'),'out':Decimal('0.0000'),'spread':Decimal('0.0000')}
 
+	for prod in PRODUCTS:
+		latestPrices[prod] = {"bid": None, "ask": None,"last":None}
+
+	print('User {0}, pw {1}, db {2}'.format(DB_USER,DB_PASSWORD,DBNAME))
+	conn = pymysql.connect(host='localhost',
+						user=DB_USER,
+						password=DB_PASSWORD, 
+						db=DBNAME,
+						cursorclass=pymysql.cursors.DictCursor)
 
 	try:
 		while True:
@@ -63,20 +78,40 @@ def trade(events):
 			else:
 				if event is not None:
 					if event.type == 'TICK':
-						# Define the current market
 						latestPrices[event.product] = {'bid':event.bid,'ask':event.ask,'last':event.price}
+
+						with conn.cursor() as cursor:
+							sql = '''INSERT INTO ticks(sequence, product, time, price, bid, ask, spread, side, size) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s)'''	
+							cursor.execute(sql,(event.sequence, event.product, event.time, event.price, event.bid, event.ask, event.spread, event.side, event.size))
+						conn.commit()
 
 						if event.product == 'ETH-BTC':
 							# Buy BTC-USD, buy ETH-BTC, sell ETH-USD
-							legz = latestPrices['ETH-USD']['ask'] / latestPrices['BTC-USD']['ask'] - latestPrices['ETH-BTC']['bid']
-							print('The {0} arbitrage is {1}'.format(event.product, legz))
+							arbitrages[event.product]['in'] = latestPrices['ETH-USD']['bid'] / latestPrices['BTC-USD']['ask'] - latestPrices['ETH-BTC']['ask']
+							# Buy ETH-USD, sell ETH-BTC, sell BTC-USD
+							arbitrages[event.product]['out'] = latestPrices['ETH-USD']['ask'] / latestPrices['BTC-USD']['bid'] - latestPrices['ETH-BTC']['bid']
+							# Arb spread
+							arbitrages[event.product]['spread'] = arbitrages[event.product]['out'] - arbitrages[event.product]['in']
+
+							with conn.cursor() as cursor:
+								sql = '''INSERT INTO arbitrage(product, time, arb_in, arb_out, arb_spread, arb_size_in, arb_size_out) VALUES(%s,%s,%s,%s,%s,%s,%s)'''	
+								cursor.execute(sql,(event.product, event.time, arbitrages[event.product]['in'], arbitrages[event.product]['out'],arbitrages[event.product]['spread'],'',''))
+							conn.commit()
+							
+
+
+							print('The {0} arbitrage is {1} / {2}  Arb Spread: {3}'.format(event.product, 
+								round(arbitrages['ETH-BTC']['in'],8), 
+								round(arbitrages['ETH-BTC']['out'],8),
+								round(arbitrages['ETH-BTC']['spread'],8)
+								))
 						elif event.sequence % 25 == 0 or event.size > 1:
 							print('This is a {0} Tick: {1} and {2} / {3}'.format(
-										event.product,
-										latestPrices[event.product]['last'],
-										latestPrices[event.product]['bid'],
-										latestPrices[event.product]['ask']
-										))
+								event.product,
+								latestPrices[event.product]['last'],
+								latestPrices[event.product]['bid'],
+								latestPrices[event.product]['ask']
+								))
 
 	except KeyboardInterrupt:
 		print('Closing Time')
--- settings.py
+++ settings.py
@@ -4,7 +4,10 @@ import os
 API_SECRET = os.environ.get('GDAX_API_SECRET', None)
 API_KEY = os.environ.get('GDAX_API_API_KEY', None)
 API_PASSPHRASE = os.environ.get('GDAX_API_PASSPHRASE', None)
+DB_USER = os.environ.get('BD_USER', None)
+DB_PASSWORD = os.environ.get('BD_PASSWORD', None)
 
 RISK_PER_TRADE = '0.1'
 PRODUCTS = ['BTC-USD','ETH-USD','ETH-BTC']
 
+DBNAME = 'cryptoDB'
